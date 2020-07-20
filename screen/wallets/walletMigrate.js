77a0bbbbab5100dc42ca46b06a12f2abee53f016
marcosrdz <marcospr@pm.me>
2020-07-20 01:09:35 -0400

    Update walletMigrate.js

--- screen/wallets/walletMigrate.js
+++ screen/wallets/walletMigrate.js
@@ -1,12 +1,12 @@
 import { AppStorage } from '../../class';
 import AsyncStorage from '@react-native-community/async-storage';
-import RNFS from 'react-native-fs';
+// import RNFS from 'react-native-fs';
 import RNSecureKeyStore, { ACCESSIBLE } from 'react-native-secure-key-store';
 import DefaultPreference from 'react-native-default-preference';
 import { Platform } from 'react-native';
 
 export default class WalletMigrate {
-  static expoDataDirectory = RNFS.DocumentDirectoryPath + '/ExponentExperienceData/%40overtorment%2Fbluewallet/RCTAsyncLocalStorage';
+  // static expoDataDirectory = RNFS.DocumentDirectoryPath + '/ExponentExperienceData/%40overtorment%2Fbluewallet/RCTAsyncLocalStorage';
 
   constructor(onComplete) {
     this.onComplete = onComplete;
@@ -38,66 +38,66 @@ export default class WalletMigrate {
 
   // 1: Migrate Document directory from Expo
   async migrateDataFromExpo() {
-    const expoDirectoryExists = await RNFS.exists(RNFS.DocumentDirectoryPath + '/ExponentExperienceData');
+    // const expoDirectoryExists = await RNFS.exists(RNFS.DocumentDirectoryPath + '/ExponentExperienceData');
 
-    if (!expoDirectoryExists) {
-      console.log('Expo data was previously migrated. Exiting migration...');
-      await this.migrateDataToSecureKeystore();
-      return;
-    }
-    try {
-      await RNFS.unlink(RNFS.DocumentDirectoryPath + '/RCTAsyncLocalStorage_V1');
-      console.log('/RCTAsyncLocalStorage_V1 has been deleted. Continuing...');
-    } catch (error) {
-      console.log(error);
-      console.log('/RCTAsyncLocalStorage_V1 does not exist. Continuing...');
-    }
-    try {
-      await RNFS.copyFile(WalletMigrate.expoDataDirectory, RNFS.DocumentDirectoryPath + '/RCTAsyncLocalStorage_V1');
-    } catch (error) {
-      console.log('An error was encountered when trying to copy Expo data to /RCTAsyncLocalStorage_V1. Exiting migration...');
-      console.log(error);
-    }
-    try {
-      await RNFS.unlink(RNFS.DocumentDirectoryPath + '/RCTAsyncLocalStorage_V1/.DS_Store');
-    } catch (error) {
-      console.log('An error was encountered when trying to delete .DS_Store. Continuing migration...');
-      console.log(error);
-    }
-    const files = await RNFS.readDir(WalletMigrate.expoDataDirectory);
-    for (const file of files) {
-      try {
-        if (file.isFile()) {
-          if (file.name === 'manifest.json') {
-            const manifestFile = await RNFS.readFile(file.path);
-            const manifestFileParsed = JSON.parse(manifestFile);
-            if ('data' in manifestFileParsed) {
-              if (typeof manifestFileParsed.data === 'string') {
-                await AsyncStorage.setItem('data', manifestFileParsed.data);
-              }
-            }
-            if ('data_encrypted' in manifestFileParsed) {
-              if (typeof manifestFileParsed.data_encrypted === 'string') {
-                await AsyncStorage.setItem('data_encrypted', manifestFileParsed.data_encrypted);
-              }
-            }
-          } else if (file.name !== 'manifest.json') {
-            const manifestFile = await RNFS.readFile(file.path);
-            const manifestFileParsed = JSON.parse(manifestFile);
-            if (typeof manifestFileParsed === 'object') await AsyncStorage.setItem('data', JSON.stringify(manifestFileParsed));
-          }
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    try {
-      await RNFS.unlink(RNFS.DocumentDirectoryPath + '/ExponentExperienceData');
-      console.log('Deleted /ExponentExperienceData.');
-    } catch (error) {
-      console.log('An error was encountered when trying to delete /ExponentExperienceData. Exiting migration...');
-      console.log(error);
-    }
+    // if (!expoDirectoryExists) {
+    //   console.log('Expo data was previously migrated. Exiting migration...');
+    //   await this.migrateDataToSecureKeystore();
+    //   return;
+    // }
+    // try {
+    //   await RNFS.unlink(RNFS.DocumentDirectoryPath + '/RCTAsyncLocalStorage_V1');
+    //   console.log('/RCTAsyncLocalStorage_V1 has been deleted. Continuing...');
+    // } catch (error) {
+    //   console.log(error);
+    //   console.log('/RCTAsyncLocalStorage_V1 does not exist. Continuing...');
+    // }
+    // try {
+    //   await RNFS.copyFile(WalletMigrate.expoDataDirectory, RNFS.DocumentDirectoryPath + '/RCTAsyncLocalStorage_V1');
+    // } catch (error) {
+    //   console.log('An error was encountered when trying to copy Expo data to /RCTAsyncLocalStorage_V1. Exiting migration...');
+    //   console.log(error);
+    // }
+    // try {
+    //   await RNFS.unlink(RNFS.DocumentDirectoryPath + '/RCTAsyncLocalStorage_V1/.DS_Store');
+    // } catch (error) {
+    //   console.log('An error was encountered when trying to delete .DS_Store. Continuing migration...');
+    //   console.log(error);
+    // }
+    // const files = await RNFS.readDir(WalletMigrate.expoDataDirectory);
+    // for (const file of files) {
+    //   try {
+    //     if (file.isFile()) {
+    //       if (file.name === 'manifest.json') {
+    //         const manifestFile = await RNFS.readFile(file.path);
+    //         const manifestFileParsed = JSON.parse(manifestFile);
+    //         if ('data' in manifestFileParsed) {
+    //           if (typeof manifestFileParsed.data === 'string') {
+    //             await AsyncStorage.setItem('data', manifestFileParsed.data);
+    //           }
+    //         }
+    //         if ('data_encrypted' in manifestFileParsed) {
+    //           if (typeof manifestFileParsed.data_encrypted === 'string') {
+    //             await AsyncStorage.setItem('data_encrypted', manifestFileParsed.data_encrypted);
+    //           }
+    //         }
+    //       } else if (file.name !== 'manifest.json') {
+    //         const manifestFile = await RNFS.readFile(file.path);
+    //         const manifestFileParsed = JSON.parse(manifestFile);
+    //         if (typeof manifestFileParsed === 'object') await AsyncStorage.setItem('data', JSON.stringify(manifestFileParsed));
+    //       }
+    //     }
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    // try {
+    //   await RNFS.unlink(RNFS.DocumentDirectoryPath + '/ExponentExperienceData');
+    //   console.log('Deleted /ExponentExperienceData.');
+    // } catch (error) {
+    //   console.log('An error was encountered when trying to delete /ExponentExperienceData. Exiting migration...');
+    //   console.log(error);
+    // }
     await this.migrateDataToSecureKeystore();
   }
 
