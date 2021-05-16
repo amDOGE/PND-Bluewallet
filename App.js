14937130bb6d9126e0b459c91d5b108805f73adc
marcosrdz <marcosrodriguez@me.com>
2021-05-15 22:13:40 -0400

    WIP

--- App.js
+++ App.js
@@ -334,7 +334,6 @@ const App = () => {
     );
   };
   return (
-    <SafeAreaProvider>
       <View style={styles.root}>
         <StatusBar barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'} backgroundColor="transparent" translucent />
         <NavigationContainer ref={navigationRef} theme={colorScheme === 'dark' ? BlueDarkTheme : BlueDefaultTheme}>
@@ -343,12 +342,7 @@ const App = () => {
           {renderClipboardContentModal()}
         </NavigationContainer>
       </View>
-      <WatchConnectivity />
-      <DeviceQuickActions />
-      <WalletImport />
-      <Biometric />
-      <WidgetCommunication />
-    </SafeAreaProvider>
+  
   );
 };
 
--- blue_modules/BlueElectrum.js
+++ blue_modules/BlueElectrum.js
@@ -9,7 +9,6 @@ const bitcoin = require('bitcoinjs-lib');
 const ElectrumClient = require('electrum-client');
 const reverse = require('buffer-reverse');
 const BigNumber = require('bignumber.js');
-const torrific = require('../blue_modules/torrific');
 const Realm = require('realm');
 
 let _realm;
@@ -96,8 +95,7 @@ async function connectMain() {
 
   try {
     console.log('begin connection:', JSON.stringify(usingPeer));
-    mainClient = new ElectrumClient(
-      usingPeer.host.endsWith('.onion') ? torrific : global.net,
+    mainClient = new ElectrumClient(global.net,
       global.tls,
       usingPeer.ssl || usingPeer.tcp,
       usingPeer.host,
@@ -800,8 +798,7 @@ module.exports.calculateBlockTime = function (height) {
  * @returns {Promise<boolean>} Whether provided host:port is a valid electrum server
  */
 module.exports.testConnection = async function (host, tcpPort, sslPort) {
-  const client = new ElectrumClient(
-    host.endsWith('.onion') ? torrific : global.net,
+  const client = new ElectrumClient(global.net,
     global.tls,
     sslPort || tcpPort,
     host,
